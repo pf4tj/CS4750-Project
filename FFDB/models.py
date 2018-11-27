df60f8c3d161fe4a7c7cebf42f4ad338f813e108
Pierce Faraone <pf4tj@virginia.edu>
2018-11-27 17:07:01 -0500

    models.py imports

--- FFDB/models.py
+++ FFDB/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import CreateView, TemplateView, DetailView, UpdateView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
 
 # Create your models here.
