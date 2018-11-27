4e8df374b63dcccd2cfc2b69ddf07d5607e72eae
Kevin Zheng <kevinzheng1997@yahoo.com>
2018-11-27 17:45:23 -0500

    Query Inputted

--- DB_Access/urls.py
+++ DB_Access/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^QT1/$', views.QT1.as_view(), name='QueryType1'),
     url(r'^QT2/$', views.QT2.as_view(), name='QueryType2'),
     url(r'^QT3/$', views.QT3.as_view(), name='QueryType3'),
+    url(r'^QT4/$', views.QT3.as_view(), name='QueryType4'),
 ]
