610b9250fcf7d5a0e8632c4426232e05dac60cfb
Kevin Zheng <kevinzheng1997@yahoo.com>
2018-11-27 18:55:15 -0500

    Admin Done

--- DB_Access/urls.py
+++ DB_Access/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
+from django.contrib import admin
 from django.urls import include, path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView, name='home'),
+    url(r'^admin/', admin.site.urls),
     url(r'^QT1/$', views.QT1.as_view(), name='QueryType1'),
     url(r'^QT2/$', views.QT2.as_view(), name='QueryType2'),
     url(r'^QT3/$', views.QT3.as_view(), name='QueryType3'),
-    url(r'^QT4/$', views.QT3.as_view(), name='QueryType4'),
+    url(r'^QT4/$', views.QT4.as_view(), name='QueryType4'),
 ]
--- FFDB/admin.py
+++ FFDB/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
+from FFDB.models import League
+from FFDB.models import Division
+from FFDB.models import Team
+from FFDB.models import Player
+from FFDB.models import Week
+from FFDB.models import Matchup
+from FFDB.models import Playerprojection
+from FFDB.models import Tradingblock
+
+class DivisionInLine(admin.TabularInline):
+    model = Division
+
+class TeamInLine(admin.TabularInline):
+    model = Team
+    
+class PlayerInLine(admin.TabularInline):
+    model = Player
+
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ['league_id', 'name', 'current_week']
+    inlines = [DivisionInLine, ]
+    
+@admin.register(Division)
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = ['division_id', 'name', 'league']
+    inlines = [TeamInLine, ]
+    
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'division']
+    inlines = [PlayerInLine, ]
+    
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['player_id', 'name', 'position', 'NFL_team', 'league_team']
+    
+@admin.register(Matchup)
+class MatchupAdmin(admin.ModelAdmin):
+    list_display = ['matchup_id', 'home_team', 'away_team', 'week']
+    
+@admin.register(Playerprojection)
+class PlayerprojectionAdmin(admin.ModelAdmin):
+    list_display = ['player']
+    
+@admin.register(Tradingblock)
+class Tradingblock(admin.ModelAdmin):
+    list_display = ['team', 'team_needs', 'team_availables', 'interested_in']
+
+
 # Register your models here.
