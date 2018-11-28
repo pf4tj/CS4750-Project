db81ce24a4ee629f06ffcdfb08d99bf61bc6983d
Kevin Zheng <kevinzheng1997@yahoo.com>
2018-11-27 20:37:17 -0500

    User Added

--- FFDB/models.py
+++ FFDB/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import MinValueValidator
 # Create your models here.
 
 POSITION_CHOICES= (
