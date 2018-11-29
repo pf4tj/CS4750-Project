dfb957687bb99e1ad861a5d9c75adb536ec18dec
Kevin Zheng <kevinzheng1997@yahoo.com>
2018-11-28 23:22:04 -0500

    Version 1

--- DB_Access/sql.py
+++ DB_Access/sql.py
@@ -2,6 +2,7 @@ import psycopg2
 
 def execute_sql(sql_statement):
     conn = None
+    result = None
     
     try:
         # connect to the PostgreSQL database
@@ -13,10 +14,35 @@ def execute_sql(sql_statement):
         # fetch data
         cur.execute(sql_statement)
         result = cur.fetchall()
+
+        # commit the changes to the database
+        conn.commit()
         
-        for x in result:
-            print(x)
+        # close communication with the database
+        cur.close()
         
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            conn.close()
+            
+    return result
+
+def execute_sql_update(sql_statement):
+    conn = None
+    
+    try:
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(host="ec2-54-227-249-201.compute-1.amazonaws.com",database="d16og879vkngbj", user="wkwzplprcpgmml", password="18c147f5bd6be4aa905ebaa44fb63b04f490e3a64307d7ba3fe84aa54a723c7f")
+       
+        # create a new cursor
+        cur = conn.cursor()
+        
+        # fetch data
+        cur.execute(sql_statement)
+
         # commit the changes to the database
         conn.commit()
         
--- DB_Access/views.py
+++ DB_Access/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.middleware.csrf import get_token
 from django.contrib.auth.decorators import login_required
 
+from DB_Access.sql import execute_sql
+from DB_Access.sql import execute_sql_update
+
 import json
 import datetime
 import pytz
@@ -27,7 +30,13 @@ def SearchMatchupFromTeam(request):
     if request.method == 'POST':
         form = SearchMatchupFromTeamForm(request.POST)
         if form.is_valid():
-            return redirect('HomePageView')
+            team = form.cleaned_data.get('team_name')
+            team_id = team.team_id
+            
+            sql = 'SELECT * FROM public."FFDB_matchup" WHERE away_team_id = ' + str(team_id) + ' OR home_team_id = ' + str(team_id)
+            result = execute_sql(sql)
+            
+            return render(request, 'QT1Result.html', {'result': result})
     else:
         form = SearchMatchupFromTeamForm()
     return render(request, 'QT1.html', {'form': form})
@@ -37,7 +46,13 @@ def SearchPlayersFromTeam(request):
     if request.method == 'POST':
         form = SearchPlayersFromTeamForm(request.POST)
         if form.is_valid():
-            return redirect('HomePageView')
+            team = form.cleaned_data.get('team_name')
+            team_id = team.team_id
+            
+            sql = 'SELECT * FROM public."FFDB_player" WHERE league_team_id = ' + str(team_id)
+            result = execute_sql(sql)
+            
+            return render(request, 'PlayersOnTeam.html', {'result': result})
     else:
         form = SearchPlayersFromTeamForm()
     return render(request, 'QT2.html', {'form': form})
@@ -58,7 +73,13 @@ def RemovePlayerFromTeam(request):
     if request.method == 'POST':
         form = RemovePlayerFromTeamForm(request.POST, user=request.user, )
         if form.is_valid():
-            return redirect('QT3.html')
+            player = form.cleaned_data.get('player_name')
+            player_id = player.player_id
+            
+            sql = 'UPDATE public."FFDB_player" SET league_team_id = NULL WHERE player_id = ' + str(player_id)
+            result = execute_sql_update(sql)
+            
+            return redirect('AddOrRemovePlayerFromTeam')
     else:
         form = RemovePlayerFromTeamForm(user=request.user)
     return render(request, 'RemovePlayer.html', {'form': form})
@@ -68,18 +89,29 @@ def AddPlayerToTeam(request):
     if request.method == 'POST':
         form = AddPlayerToTeamForm(request.POST)
         if form.is_valid():
-            return redirect('QT3.html')
+            player = form.cleaned_data.get('player_name')
+            player_id = player.player_id
+            
+            sql = 'UPDATE public."FFDB_player" SET league_team_id = ' + str(Team.objects.get(owner=Our_User.objects.get(user=request.user)).team_id) + 'WHERE player_id = ' + str(player_id)
+            result = execute_sql_update(sql)
+            
+            return redirect('AddOrRemovePlayerFromTeam')
     else:
         form = AddPlayerToTeamForm()
     return render(request, 'AddPlayer.html', {'form': form})
 
 @login_required
 def QT4(request):
-    return render(request, 'QT4.html')
+    sql = 'SELECT * FROM public."FFDB_team" WHERE team_id IN (SELECT home_team_id FROM public."FFDB_matchup" WHERE home_team_points > away_team_points)'
+    result = execute_sql(sql)
+            
+    return render(request, 'QT4.html', {'result': result})
 
 @login_required
 def PlayersWithoutTeam(request):
-    return render(request, 'PlayersWithoutTeam.html')
+    sql = 'SELECT * FROM public."FFDB_player" WHERE league_team_id IS NULL'
+    result = execute_sql(sql)
+    return render(request, 'PlayersWithoutTeam.html', {'result': result})
 
 @login_required
 def logout_user(request):
